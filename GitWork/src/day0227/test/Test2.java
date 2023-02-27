40e7aa7603cc35e1e11b251fc081ec8167e5b863
kimjaeyou <102010656+kimjaeyou@users.noreply.github.com>
2023-02-27 15:14:41 +0900

    Create Test2.java

---
+++ GitWork/src/day0227/test/Test2.java
@@ -0,0 +1,10 @@
+package day0227.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is testfile2");
+	}
+
+}
